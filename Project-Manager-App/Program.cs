87e33e46f135d35adb0bb22904712ae5bea9b178
Ayden Smith <s_ayden@ymail.com>
2025-03-11 16:27:54 +1100

    Added Auth0 authentication login page.

--- Project-Manager-App/Program.cs
+++ Project-Manager-App/Program.cs
@@ -1,9 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using Project_Manager_App.Components;
 using Project_Manager_App.Data;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Get Connection string to PostgreSQL connection
 var connection = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+// Cookie configuration for HTTPS
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+// Add Auth0 Authentication
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
+builder.Services.AddControllersWithViews();
+builder.Services.AddCascadingAuthenticationState();
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -27,6 +48,32 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+// Map Login Route
+app.MapGet("/Account/Login", async (HttpContext httpContext, string returnUrl = "/") =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(returnUrl)
+            .Build();
+
+    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+// Map logout route
+app.MapGet("/Account/Logout", async (HttpContext httpContext) =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri("/")
+            .Build();
+
+    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
+
+// Use authentication & authorizaiton via Auth0
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
